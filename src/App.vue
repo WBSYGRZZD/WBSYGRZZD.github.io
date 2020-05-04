48df0480e090bcfb9de1dc8ab70aa7d5133df85f
xkdy <17314825504@163.com>
2020-05-04 16:46:48 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,16 @@
         <router-link to="/Home">首页</router-link>
       </el-breadcrumb-item>
       <el-breadcrumb-item>
-        <router-link to="/ShoppingCart">购物车</router-link>
-      </el-breadcrumb-item>
-      <el-breadcrumb-item>
         <router-link to="/Backstage">后台</router-link>
       </el-breadcrumb-item>
+      <div v-if="true">
+        <el-breadcrumb-item>
+          <router-link to="/ShoppingCart">购物车</router-link>
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>
+          <el-button type="text" @click="login">未登录</el-button>
+        </el-breadcrumb-item>
+      </div>
     </el-breadcrumb>
     <router-view />
   </div>
